90b44a2980d34e01f9b3c404f57959ac9330ab44
Ripperoni <lcsabi9d@gmail.com>
2020-10-05 12:39:58 +0200

    Add project files.

---
+++ oraimunka1005/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oraimunka1005
+{
+    public partial class frmFo : Form
+    {
+        private int Aoldal;
+        private int Boldal;
+        private int Coldal;
+        public frmFo()
+        {
+            Aoldal = 0;
+            Boldal = 0;
+            Coldal = 0;
+            InitializeComponent();
+            tbAoldal.Text = Aoldal.ToString();
+            tbBoldal.Text = Boldal.ToString();
+            tbColdal.Text = Coldal.ToString();
+            lbHarmszogLista.Items.Clear();
+        }
+
+        private void btnKilepes_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSzamol_Click(object sender, EventArgs e)
+        {
+            Aoldal = Convert.ToInt32(tbAoldal.Text);
+            Boldal = Convert.ToInt32(tbBoldal.Text);
+            Coldal = Convert.ToInt32(tbColdal.Text);
+           
+           
+            if (Aoldal == 0 || Boldal == 0 || Coldal == 0)
+            {
+                MessageBox.Show("A háromszög oldala nem lehet 0", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                var h = new Haromszog(Aoldal, Boldal, Coldal);
+                
+                List<string> adatok = h.AdatokSzoveg();
+                foreach (var a in adatok)
+                {
+                    lbHarmszogLista.Items.Add(a);
+                }
+            }
+        }
+
+        private void tbAoldal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            if (lbHarmszogLista.Items.Count > 0)
+            {
+                lbHarmszogLista.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Nincs mit törölni.");
+            }
+            
+        }
+    }
+}
---
+++ oraimunka1005/Haromszog.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oraimunka1005
+{
+    internal class Haromszog
+    {
+        private int aoldal;
+        private int boldal;
+        private int coldal;
+
+        public double Terulet { get; private set; }
+        public double Kerulet { get; private set; }
+        public bool Szerkesztheto { get; private set; }
+
+        public List<string> AdatokSzoveg()
+        {
+            List<string> adatok = new List<string>();
+            adatok.Add($"A: {aoldal} - B: {boldal} - C: {coldal}");
+             adatok.Add($"Kerület: {Kerulet} - Terület: {Terulet}");
+            if (Szerkesztheto)
+            {
+                adatok.Add($"Kerület: {Kerulet} - Terület: {Terulet}");
+            }
+            else
+            {
+                adatok.Add("Nem szerkeszthető");
+            }
+            return adatok;
+        }
+        private double Teruletszamitas()
+        {
+            double s = (aoldal + boldal + coldal) / 2;
+            double ter = Math.Sqrt(s * (s - aoldal) * (s - boldal) * (s - coldal));
+            return ter;
+        }
+        private double Keruletszamitas()
+        {
+            double ker = aoldal + boldal + coldal;
+            return ker;
+        }
+
+        private void szerk()
+        {
+            if (aoldal + boldal > coldal && aoldal + coldal > boldal && boldal + coldal > aoldal)
+            {
+                Szerkesztheto = true;
+                Terulet = Teruletszamitas();
+                Kerulet = Keruletszamitas();
+                
+            }
+            else
+            {
+                Szerkesztheto = false;
+                Terulet = 0;
+                Kerulet = 0;
+            }
+        }
+        public Haromszog(int aoldal, int boldal, int coldal)
+        {
+            this.aoldal = aoldal;
+            this.boldal = boldal;
+            this.coldal = coldal;
+            szerk();
+        }
+    }
+}
